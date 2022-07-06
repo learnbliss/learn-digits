5014b50540079d41011a00a9efd56b26037a58ec
Dmitriy Petrukhin <petrukhindmitriy@gmail.com>
2022-07-06 23:54:01 +0300

    fix disable multiple click

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,17 +1,18 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styles from './App.module.css'
 import Item from "../Item";
 
 function App() {
-    // const [, set] = useState(initial);
+    const [block, setBlock] = useState(false);
     return (
         <>
             <div className={styles.container}>
-                {[...new Array(101)].map((_, i) => <Item digit={String(i)} color={i % 2 ? '#25A17E' : '#CB1D35'}>{i}</Item>)}
+                {[...new Array(101)].map((_, i) => <Item block={block} setBlock={setBlock} digit={String(i)} color={i % 2 ? '#25A17E' : '#CB1D35'}>{i}</Item>)}
             </div>
             <div>
                 <button>Settings</button>
             </div>
+            {block && <div className={styles.blocked}/>}
         </>
     );
 }
--- src/components/Item/Item.tsx
+++ src/components/Item/Item.tsx
@@ -1,17 +1,32 @@
-import React from 'react';
+import React, {useMemo} from 'react';
 import styles from './Item.module.css'
 
 type ItemProps = {
     color: string;
     children: React.ReactNode;
     digit: string;
+    block: boolean;
+    setBlock: (bool: boolean) => void;
 }
 
-const Item: React.FC<ItemProps> = ({color, children, digit}) => {
+const Item: React.FC<ItemProps> = ({color, children, digit,block,  setBlock}) => {
+    const synth = speechSynthesis;
+    const voice = useMemo(
+        () =>
+            new SpeechSynthesisUtterance(),
+        []
+    )
+    voice.rate = 0.1;
+
     const handleVoice = () => {
-        speechSynthesis.speak(
-            new SpeechSynthesisUtterance(digit)
-        );
+        if (block) {
+            return
+        }
+        voice.onstart = () => setBlock(true)
+        synth.cancel();
+        voice.text = digit;
+        synth.speak(voice);
+        voice.onend = () => setBlock(false)
     }
     return (
         <div className={styles.root} style={{color: color}}>
